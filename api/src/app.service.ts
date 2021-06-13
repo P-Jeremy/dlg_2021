520fd7b1d3b9e4645e86492da8108cf020449349
P-Jeremy <jemyplu@gmail.com>
2021-06-13 20:01:14 +0200

    test trigger only api action on api change

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test deploy on api change';
+    return 'Test: Only api github action should trigger';
   }
 }
