e0a30e4330a62a010a707990263774e0d25e90bb
P-Jeremy <jemyplu@gmail.com>
2021-06-13 20:04:24 +0200

    test trigger only front action on front change

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Test deploy change 1<h2>
+    <h2>Test trigger only front github action<h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
