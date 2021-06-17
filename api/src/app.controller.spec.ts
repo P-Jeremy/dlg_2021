48a6d48d87fe0e956e7cf20563eb0dbd8399e3c1
P-Jeremy <jemyplu@gmail.com>
2021-06-17 15:57:32 +0200

    Adding circleci continuous integration

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe(
+        'Test: Only api github action should trigger',
+      );
     });
   });
 });
