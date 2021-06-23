6995ab58e76e63c1a2067ef7b32a2d3a067ae1f2
P-Jeremy <jemyplu@gmail.com>
2021-06-23 12:50:36 +0200

    [API] Add unit test on get all songs

---
+++ api/src/songs/get-songs/test/unit/usecase/get-songs_tests.unit.spec.ts
@@ -0,0 +1,60 @@
+import { Song } from '../../../domain/models/Song';
+import { getAllSongs } from '../../../domain/usecase/get-all-songs';
+
+describe('UNIT | USECASE | get-songs', () => {
+  describe('When there are songs in the db', () => {
+    it('returns all the songs', async () => {
+      // given
+      const songs = [
+        Song.from({
+          title: 'Tata yoyo',
+          author: 'Pr Tournsol',
+          lyrics: 'Taaaata yoyo',
+          tab: 'http.tabs.fr',
+          tags: [],
+        }),
+        Song.from({
+          title: 'Big bisou',
+          author: 'Tintin',
+          lyrics: 'Big bisooooou',
+          tab: 'http.tabs.fr',
+          tags: [],
+        }),
+      ];
+
+      const songRepository = {
+        getAll: jest.fn(async () => songs),
+      };
+
+      // when
+      const result = await getAllSongs({
+        dependencies: {
+          songRepository,
+        },
+      });
+
+      // then
+      expect(result).toBe(songs);
+    });
+  });
+  describe('When there are no songs in the db', () => {
+    it('returns an empty array', async () => {
+      // given
+      const noSongFound = [];
+
+      const songRepository = {
+        getAll: jest.fn(async () => noSongFound),
+      };
+
+      // when
+      const result = await getAllSongs({
+        dependencies: {
+          songRepository,
+        },
+      });
+
+      // then
+      expect(result).toEqual([]);
+    });
+  });
+});
